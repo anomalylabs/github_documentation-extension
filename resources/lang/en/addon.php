4a9b181d9e88632172fb51931b824524a1400b0b
Ryan Thompson <ryan@pyrocms.com>
2016-03-24 01:48:45 -0500

    M

--- resources/lang/en/addon.php
+++ resources/lang/en/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Github Documentation',
+    'title'       => 'Github',
     'name'        => 'Github Documentation Extension',
     'description' => 'A GitHub project source for the documentation module.'
 ];
